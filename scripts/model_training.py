1d969c73631191b4b698b53ace3f467010e06eca
Jakub Kondek <j.kondek@telekom.com>
2025-08-02 13:39:31 +0200

    modify training scripts

--- scripts/model_training.py
+++ scripts/model_training.py
@@ -1,25 +1,33 @@
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
 
-df_rbb['reviews'] = df_rbb['reviews'].str.replace(',', '').astype(int)
-encoder = OneHotEncoder(sparse_output=False)
-df_rbb_address_0_ohe = encoder.fit_transform(df_rbb[['address_0']])
-df_rbb_address_0_ohe = pd.DataFrame(
-    df_rbb_address_0_ohe,
-    columns=encoder.get_feature_names_out()
-)
-df_rbb = pd.concat([df_rbb, df_rbb_address_0_ohe], axis=1)
+def modify_pre_training(data: pd.DataFrame) -> pd.DataFrame:
+    data['reviews'] = data['reviews'].str.replace(',', '').astype(int)
+    return data
 
-df_rbb
-#%%
-X_train, X_test, y_train, y_test = train_test_split(
-    df_rbb.drop(columns=['price']),
-    df_rbb['price'],
-    test_size=0.2,
-    random_state=42
-)
-model_lr = LinearRegression()
-model_lr.fit(X_train, y_train)
+def encode(data: pd.DataFrame, encoder) -> pd.DataFrame:
+    data_address_0_ohe = encoder.fit_transform(data[['address_0']])
+    data_address_0_ohe = pd.DataFrame(
+        data_address_0_ohe,
+        columns=encoder.get_feature_names_out()
+    )
+    data = pd.concat([data, data_address_0_ohe], axis=1)
+    return data
+
+#encoder = OneHotEncoder(sparse_output=False)
+
+def create_train_test_split(data: pd.DataFrame) -> tuple:
+    X_train, X_test, y_train, y_test = train_test_split(
+        data.drop(columns=['price']),
+        data['price'],
+        test_size=0.2,
+        random_state=42
+    )
+    return X_train, X_test, y_train, y_test
+
+def fit_model(train, label, model):
+    model.fit(train, label)
+    return model
+
+#model_lr = LinearRegression()
+#model_lr.fit(X_train, y_train)
