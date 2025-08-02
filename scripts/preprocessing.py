565c2e9fc89e22a272faa449626903f78c41a5dd
Jakub Kondek <j.kondek@telekom.com>
2025-08-02 13:34:16 +0200

    modify preprocessing functions

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,50 +1,43 @@
+import pandas as pd
+import numpy as np
 
-df_host_counts = df_rbb.groupby('host_id')['name'].nunique().rename(
-    'host_count'
-)
-
-df_rbb['host_count'] = df_host_counts.loc[df_rbb['host_id']].values
-
-cols_to_drop = [
+COLS_TO_DROP = [
     'id',
     'name',
-    # 'rating',
-    # 'reviews',
     'host_name',
     'host_id',
-    # 'address',
     'features',
     'amenities',
     'safety_rules',
     'hourse_rules',
     'img_links',
-    # 'price',
     'country',
-    # 'bathrooms',
-    # 'beds',
-    # 'guests',
-    # 'toiles',
-    # 'bedrooms',
-    # 'studios',
-    # 'checkin',
-    # 'checkout'
 ]
 
-df_rbb = df_rbb.drop(columns=cols_to_drop)
+def create_host_counts(data: pd.DataFrame) -> pd.DataFrame:
+    df_host_counts = data.groupby('host_id')['name'].nunique().rename('host_count')
+    data['host_count'] = df_host_counts.loc[data['host_id']].values
+    return data
 
-df_rbb['address_0'] = df_rbb['address'].str.split(',').str[0]
-df_rbb_address_0_counter = df_rbb['address_0'].value_counts().loc[df_rbb['address_0']]
-df_rbb['address_0'] = np.where(df_rbb_address_0_counter > 50, df_rbb['address_0'], 'other')
-df_rbb = df_rbb.drop(columns=['address', 'checkout', 'checkin'])
-df_rbb_rating = np.where(
-    df_rbb['rating'] == 'New',
-    np.NaN,
-    df_rbb['rating']
-)
-df_rbb_rating_mean = pd.Series(df_rbb_rating.astype(float)).dropna().mean()
-df_rbb_rating = np.where(
-    pd.Series(df_rbb_rating).isna(),
-    df_rbb_rating_mean,
-    df_rbb['rating']
-).astype(float)
-df_rbb['rating'] = df_rbb_rating
+def drop_cols(data: pd.DataFrame, cols: list = COLS_TO_DROP) -> pd.DataFrame:
+    data = data.drop(columns=cols)
+    return data
+
+def modify_other_stuff(data: pd.DataFrame) -> pd.DataFrame:
+    data['address_0'] = data['address'].str.split(',').str[0]
+    df_rbb_address_0_counter = data['address_0'].value_counts().loc[data['address_0']]
+    data['address_0'] = np.where(df_rbb_address_0_counter > 50, data['address_0'], 'other')
+    data = data.drop(columns=['address', 'checkout', 'checkin'])
+    df_rbb_rating = np.where(
+        data['rating'] == 'New',
+        np.NaN,
+        data['rating']
+    )
+    df_rbb_rating_mean = pd.Series(df_rbb_rating.astype(float)).dropna().mean()
+    df_rbb_rating = np.where(
+        pd.Series(df_rbb_rating).isna(),
+        df_rbb_rating_mean,
+        data['rating']
+    ).astype(float)
+    data['rating'] = df_rbb_rating
+    return data
